9311c70b41190be11d2bee695f2331e601123ad6
Cian O'Gralaigh <ihatecian@gmail.com>
2018-09-25 19:15:28 +0100

    Initialise project"

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./css/main.css";
+import { Jumbotron } from "reactstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
